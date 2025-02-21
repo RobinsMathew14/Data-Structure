ed6a4d71d8176938bc7adb3fdf20b1c5b3c23c2e
Robins Mathew <145858011+RobinsMathew14@users.noreply.github.com>
2025-02-21 12:26:07 +0530

    seamaphore.c

---
+++ seamaphore.c
@@ -0,0 +1,58 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+int mutex = 1;
+int full = 0;
+int empty, x = 0;
+
+void producer(){
+    --mutex;
+    ++full;
+    --empty;
+    x++;
+    printf("\nProducer produces item %d", x);
+    ++mutex;
+}
+
+void consumer(){
+    --mutex;
+    --full;
+    ++empty;
+    printf("\nConsumer consumes item %d", x);
+    x--;
+    ++mutex;
+}
+
+int main(){
+    int n, i, s;
+    printf("Enter the size of the Buffer");
+    scanf("%d", &s);
+    empty = s;
+    printf("\n1. Process 1 for producer \n2. Process 2 for consumer \n3. Process 3 for exit\n");
+
+    for(i = 1; i > 0; i++){
+        printf("\nEnter your choice ");
+        scanf("%d", &n);
+        switch(n){
+            case 1:
+                if((mutex == 1) && (empty != 0)){
+                    producer();
+                } else {
+                    printf("Buffer is full");
+                }
+                break;
+
+            case 2:
+                if((mutex == 1) && (full != 0)){
+                    consumer();
+                } else {
+                    printf("Buffer is empty");
+                }
+                break;
+
+            case 3:
+                exit(0);
+                break;
+        }
+    }
+}
